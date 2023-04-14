1a1175dd1fafca00cacd6497ee7d27e3253d1d61
LeeDongWoo99 <dleh684@gmail.com>
2023-04-14 10:42:13 +0900

    [Bronze II] Title: 분해합, Time: 1396 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,10 @@
+N = int(input())
+result = 0
+for i in range(1,N+1):
+    A = list(map(int, str(i)))
+    result = sum(A) + i
+    if result == N:
+        print(i)
+        break
+    if N == i:
+        print(0)
