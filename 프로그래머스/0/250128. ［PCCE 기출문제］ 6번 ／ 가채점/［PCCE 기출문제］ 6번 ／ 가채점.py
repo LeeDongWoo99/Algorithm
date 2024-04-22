f33668ec0dce84a5a7351fd191d6a0853746549f
dongwooLee <dleh684@gmail.com>
2024-04-22 20:29:54 +0900

    Update ［PCCE 기출문제］ 6번 ／ 가채점.py
    복습

--- 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.py
+++ 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.py
@@ -7,3 +7,22 @@ def solution(numbers, our_score, score_list):
             answer.append("Different")
     
     return answer
+
+-------------------------------------------------------------------------
+위에 풀이는 프로그래머스 특성상 한 줄만 수정해서 맞추면 정답이 되기 때문에 문제만 보고 다시 코딩을 했다.
+    
+def solution(numbers, our_score, score_list):
+    result = []
+    for i in range(len(numbers)):
+        if our_score[i] == score_list[numbers[i]-1]:
+            result.append("Same")
+        else:
+            result.append("Different")
+    return result
+      
+numbers = [3, 4]
+our_score = [85, 93]
+score_list = [85, 92, 38, 93, 48, 85, 92, 56]
+
+result = solution(numbers, our_score, score_list)
+print(result)
