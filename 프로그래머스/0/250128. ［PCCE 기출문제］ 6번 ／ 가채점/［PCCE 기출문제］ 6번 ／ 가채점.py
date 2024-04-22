cf00157ef3c48dbe5f1f8b4d9d4fe9ee8d5687b5
dongwooLee <dleh684@gmail.com>
2024-04-22 20:05:09 +0900

    [level 0] Title: [PCCE 기출문제] 6번 / 가채점, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.py
+++ 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.py
@@ -1,7 +1,7 @@
 def solution(numbers, our_score, score_list):
     answer = []
     for i in range(len(numbers)):
-        if score_list[numbers[i]-1] == our_score[i]:
+        if our_score[i] == score_list[numbers[i]-1]:
             answer.append("Same")
         else:
             answer.append("Different")
