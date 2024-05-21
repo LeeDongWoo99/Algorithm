dec72622524ac06941b1ad2f5f09829935cd2bcc
dongwooLee <dleh684@gmail.com>
2024-05-21 10:41:43 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,7 +1,12 @@
 # 마름모 변 입력받기
 n = int(input())
 N = 2 * n - 1
-for i in range(0, n):
-    print(" " * (n - i - 1) + "*" * ((2 * i) + 1))
-for j in range(1, n):
-    print(" " * j + "*" * (N - (2 * j)))
+ans = 1
+cnt = 1
+while ans < n + 1:
+    if cnt < n + 1:
+        print(" " * (n - cnt) + "*" * (2 * cnt - 1))
+        cnt += 1
+    else:
+        print(" " * ans + "*" * (2 * n - 2 * ans - 1))
+        ans += 1
