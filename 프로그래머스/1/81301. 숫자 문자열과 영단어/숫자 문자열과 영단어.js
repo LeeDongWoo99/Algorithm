8bdd5577e3a57662ef7ca58e90cf547356f8d405
dongwooLee <dleh684@gmail.com>
2024-05-29 16:11:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,21 @@
-function solution(s) {
-    let num = ["zero", "one", "two", "three", "four", "five", "six",
-               "seven","eight","nine"] 
-    var lst = s
-    
-    for (let i = 0; i <num.length; i++) {
-        let arr = lst.split(num[i]);
-        lst = arr.join(i)
+function solution(input) {
+    let s = input;  // 여기서 s를 명시적으로 선언합니다.
+    const num = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    };
+
+    for (let key in num) {
+        s = s.split(key).join(num[key]);
     }
-    return Number(lst);
+
+    return parseInt(s, 10);
 }
