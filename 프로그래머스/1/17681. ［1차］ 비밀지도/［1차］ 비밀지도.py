87c795d33b4848ce0e67137ca8b6b60f0b4dee21
dongwooLee <dleh684@gmail.com>
2024-05-30 20:38:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,19 +2,9 @@ def solution(n, arr1, arr2):
     ans = []
     
     for i in range(n):
-        # 각 숫자를 이진수로 변환하고 n 길이만큼 앞에 0을 추가
-        bin1 = format(arr1[i], 'b').zfill(n)
-        bin2 = format(arr2[i], 'b').zfill(n)
-        
-        # 두 이진수를 OR 연산하여 결합
-        combined = int(bin1, 2) | int(bin2, 2)
-        
-        # 결합한 결과를 이진수 문자열로 변환하고 앞에 0을 추가
-        combined_bin = format(combined, 'b').zfill(n)
-        
-        # 이진수 문자열을 "#"과 " "로 변환
-        row = combined_bin.replace("0", " ").replace("1", "#")
-        
-        ans.append(row)
-    
+        chk = int(arr1[i]) | int(arr2[i])
+        lst = bin(chk)[2:].zfill(n)  # 이진수로 변환하고 앞에 0을 채워 길이를 맞춤
+        lst = lst.replace("0", " ").replace("1", "#")
+        ans.append(lst)
     return ans
+    
