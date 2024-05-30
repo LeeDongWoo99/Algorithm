4d77ea02b0d384bcfb0a4a52c4a294b7d7bd54df
dongwooLee <dleh684@gmail.com>
2024-05-30 22:45:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.20 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,11 +1,22 @@
 function solution(n, arr1, arr2) {
     let ans = [];
-
+    
     for (let i = 0; i < n; i++) {
-        let chk = arr1[i] | arr2[i];
-        let lst = chk.toString(2).padStart(n, '0'); // 2진수로 변환하고 앞에 0을 채워 길이를 맞춤
-        lst = lst.replace(/0/g, ' ').replace(/1/g, '#');
-        ans.push(lst);
+        // 각 숫자를 이진수로 변환하고 n 길이만큼 앞에 0을 추가
+        let bin1 = arr1[i].toString(2).padStart(n, '0');
+        let bin2 = arr2[i].toString(2).padStart(n, '0');
+        
+        // 두 이진수를 OR 연산하여 결합
+        let combined = "";
+        for (let j = 0; j < n; j++) {
+            if (bin1[j] === '1' || bin2[j] === '1') {
+                combined += '#';
+            } else {
+                combined += ' ';
+            }
+        }
+        
+        ans.push(combined);
     }
     
     return ans;
