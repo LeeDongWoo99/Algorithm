3060cba6979f35d381e978ae3c0f639cc5846d48
dongwooLee <dleh684@gmail.com>
2024-05-30 21:04:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,7 +2,7 @@ def solution(n, arr1, arr2):
     ans = []
     
     for i in range(n):
-        chk = int(arr1[i]) | int(arr2[i])
+        chk = arr1[i] | arr2[i]
         lst = bin(chk)[2:].zfill(n)  # 이진수로 변환하고 앞에 0을 채워 길이를 맞춤
         lst = lst.replace("0", " ").replace("1", "#")
         ans.append(lst)
