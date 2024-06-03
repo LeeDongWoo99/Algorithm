b62e9b468b63b5be39b6fbf0591167a093a3991c
dongwooLee <dleh684@gmail.com>
2024-06-03 21:22:54 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,15 +1,17 @@
 def solution(board, moves):
-    result = 0
     basket = []
+    result = 0
     for move in moves:
         for row in board:
+            # 현재 행에서 크레인 이동 방향에 인형이 있을 경우
             if row[move-1] != 0:
                 doll = row[move-1]
                 row[move-1] = 0
                 if basket and basket[-1] == doll:
-                    result +=2
+                    result += 2
                     basket.pop()
                 else:
                     basket.append(doll)
                 break
+            
     return result
