bc035a67ff44433d844100d4902ea283067af9cd
dongwooLee <dleh684@gmail.com>
2024-06-05 10:03:37 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.32 ms
+메모리: 10.4 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 22:23:55
+2024년 06월 05일 10:03:34
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,33 +1,29 @@
 def solution(numbers, hand):
-    # 키패드를 2차원 배열로 정의합니다.
-    keypad = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        ['*', 0, '#']
-    ]
-    
-    # 손의 현재 위치를 저장할 변수를 정의합니다. 시작 위치는 '*'와 '#'입니다.
-    left_pos = (3, 0)
-    right_pos = (3, 2)
-    
     result = ""
+    left_pos = (3, 0)  # '*'의 초기 위치
+    right_pos = (3, 2)  # '#'의 초기 위치
+    hand = "L" if hand == "left" else "R"
 
-    # 키패드에서 각 숫자의 위치를 저장하는 딕셔너리를 만듭니다.
-    pos_dict = {keypad[row][col]: (row, col) for row in range(4) for col in range(3)}
+    # 키패드의 좌표 정의
+    keypad_pos = {
+        1: (0, 0), 2: (0, 1), 3: (0, 2),
+        4: (1, 0), 5: (1, 1), 6: (1, 2),
+        7: (2, 0), 8: (2, 1), 9: (2, 2),
+        0: (3, 1)
+    }
 
     for num in numbers:
         if num in [1, 4, 7]:
             result += "L"
-            left_pos = pos_dict[num]
+            left_pos = keypad_pos[num]
         elif num in [3, 6, 9]:
             result += "R"
-            right_pos = pos_dict[num]
+            right_pos = keypad_pos[num]
         else:
-            target_pos = pos_dict[num]
+            target_pos = keypad_pos[num]
             left_dist = abs(left_pos[0] - target_pos[0]) + abs(left_pos[1] - target_pos[1])
             right_dist = abs(right_pos[0] - target_pos[0]) + abs(right_pos[1] - target_pos[1])
-            
+
             if left_dist < right_dist:
                 result += "L"
                 left_pos = target_pos
@@ -35,7 +31,7 @@ def solution(numbers, hand):
                 result += "R"
                 right_pos = target_pos
             else:
-                if hand == "left":
+                if hand == "L":
                     result += "L"
                     left_pos = target_pos
                 else:
