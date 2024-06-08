0696f7238cbf23f02b7e74eadeb6361203f7b95e
dongwooLee <dleh684@gmail.com>
2024-06-08 10:32:18 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,19 +1,17 @@
 def solution(skill, skill_trees):
     result = 0
-
     for skill_tree in skill_trees:
-        skill_index = 0  # 현재 검사해야 할 스킬의 인덱스
-        valid = True  # 스킬 트리가 유효한지 여부를 나타내는 플래그
-
-        for s in skill_tree:
-            if s in skill:
-                if s == skill[skill_index]:  # 올바른 순서로 배우고 있는 경우
-                    skill_index += 1
-                else:  # 순서가 잘못된 경우
-                    valid = False
-                    break
-
-        if valid:
-            result += 1
+        check_list = []
+        for sk in skill_tree:
+            if sk in skill:
+                check_list.append(sk)
+                
+        is_valid = True
+        for i in range(len(check_list)):
+            if check_list[i] != skill[i]:
+                is_valid = False
+                break
+        if is_valid:
+            result +=1
 
     return result
