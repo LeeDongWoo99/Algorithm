cccb5fd0a5294de893c7f685c925d2a988ec3807
dongwooLee <dleh684@gmail.com>
2024-06-11 10:53:41 +0900

    [Silver V] Title: 집합, Time: 3384 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,37 +2,39 @@ import sys
 input = sys.stdin.readline
 
 # 수행해야 하는 연산의 수
-M = int(input().strip())
-S = set()
+M = int(input())
+S = []
 
 for _ in range(M):
-    order = input().strip().split()
-    # 리스트에 명령어 부분만 추출
+    order = input().split()
     ord = order[0]
 
-    if len(order) > 1:
-        x = int(order[1])
-
     if ord == "add":
-        S.add(x)
+        x = int(order[1])
+        if x not in S:
+            S.append(x)
 
     elif ord == "remove":
-        S.discard(x)  # discard를 사용하면 try-except가 필요 없습니다.
+        x = int(order[1])
+        if x in S:
+            S.remove(x)
 
     elif ord == "check":
+        x = int(order[1])
         if x in S:
             print(1)
         else:
             print(0)
 
     elif ord == "toggle":
+        x = int(order[1])
         if x in S:
             S.remove(x)
         else:
-            S.add(x)
+            S.append(x)
 
     elif ord == "all":
-        S = set(range(1, 21))
+        S = [i for i in range(1, 21)]
 
     elif ord == "empty":
-        S.clear()
+        S = []
