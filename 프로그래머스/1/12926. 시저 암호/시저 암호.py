3f0f7ef81e224a78e2f315aa591a1c83c3499074
dongwooLee <dleh684@gmail.com>
2024-06-12 18:00:16 +0900

    [level 1] Title: 시저 암호, Time: 1.96 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,14 +1,14 @@
 def solution(s, n):
-    low = "abcdefghijklmnopqrstuvwxyz" # 소문자. 인덱스는 0에서 25
+    low = "abcdefghijklmnopqrstuvwxyz" # 
     up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     answer = ''
-    for char in s:
-        if char in low:
-            ind = low.index(char)+n # low 문자열에서 찾은 해당 알파벳 인덱스 + n
-            answer += low[ind%26] # 26으로 나눈 나머지를 사용할 경우 25를 초과하는 경우도 활용 가능
-        elif char in up:
-            ind = up.index(char)+n
-            answer += up[ind%26]
-        else: # 공백일 경우도 고려
+    for ch in s:
+        if ch.islower():
+            ind = (low.index(ch)+n) % 26
+            answer += low[ind] 
+        elif ch.isupper():
+            ind = (up.index(ch)+n) % 26
+            answer += up[ind]
+        else: # 공백일 경우 공백 추가
             answer += " "
     return answer
