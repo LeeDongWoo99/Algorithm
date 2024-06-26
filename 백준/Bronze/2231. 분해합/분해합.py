3bda225489d56bcdb932076f20b16c8f0a03e42a
dongwooLee <dleh684@gmail.com>
2024-06-26 11:45:46 +0900

    [Bronze II] Title: 분해합, Time: 356 ms, Memory: 110680 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,12 @@
 N = int(input())
+
 result = 0
-for i in range(1,N+1):
-    A = list(map(int, str(i)))
-    result = sum(A) + i
+
+for num in range(1, N+1):
+    lst = list(map(int, str(num)))
+    result = sum(lst) + num
     if result == N:
-        print(i)
+        print(num)
         break
-    if N == i:
+    if N == num: # 입력값과 num이 같다는 것은, 반복문이 끝날때 까지 생성자 값이 안나왔다는 뜻이다.
         print(0)
