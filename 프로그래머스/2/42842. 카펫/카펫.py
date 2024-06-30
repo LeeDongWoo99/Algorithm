46870df9306476fe47a7a38a77d301d5dac7bddc
dongwooLee <dleh684@gmail.com>
2024-06-30 20:40:17 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,8 @@
 def solution(brown, yellow):
-    total = brown + yellow  # 전체 타일 수
-    
-    # 전체 타일 수의 약수 구하기
-    for height in range(1, int(total ** 0.5) + 1):
-        if total % height == 0:
-            width = total // height
-            # 갈색 테두리 타일 고려한 노란색 타일 크기 확인
-            if (width - 2) * (height - 2) == yellow:
-                return [width, height]
+    total = brown + yellow
+    ans = []
+    for i in range(1, int(total ** 0.5) +1):
+        if total % i == 0:
+            width = total // i
+            if (width -2) * (i - 2) == yellow:
+                return [width, i]
