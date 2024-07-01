79725d20262b7331171d2733e9a812452f45c4ca
dongwooLee <dleh684@gmail.com>
2024-07-01 22:56:51 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,7 @@
 def solution(brown, yellow):
     total = brown + yellow
-    ans = []
-    for i in range(1, int(total ** 0.5) +1):
-        if total % i == 0:
-            width = total // i
-            if (width -2) * (i - 2) == yellow:
-                return [width, i]
+    for height in range(1, int(total ** 0.5) +1):
+        if total % height == 0:
+            width = total // height
+            if (width - 2) * (height - 2) == yellow:
+                return [width, height]
