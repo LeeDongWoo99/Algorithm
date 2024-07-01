96ce510ac4ad3e694ffa04d5e8aef28dcb108a00
dongwooLee <dleh684@gmail.com>
2024-07-01 10:26:20 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,16 +1,19 @@
 def solution(k, dungeons):
-    max_dungeons = 0
-    
+    max_count = 0
+
     def dfs(current_k, visited, count):
-        nonlocal max_dungeons
-        max_dungeons = max(max_dungeons, count)
-        
+        nonlocal max_count
+        max_count = max(max_count, count)
+
         for i in range(len(dungeons)):
             if not visited[i] and current_k >= dungeons[i][0]:
                 visited[i] = True
                 dfs(current_k - dungeons[i][1], visited, count + 1)
                 visited[i] = False
+                
     visited = [False] * len(dungeons)
     dfs(k, visited, 0)
-
-    return max_dungeons
+    
+    return max_count
+        
+    
