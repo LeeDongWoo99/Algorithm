cfcea74c93991b864fbac90379254057573efc30
dongwooLee <dleh684@gmail.com>
2024-07-02 20:06:10 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,27 +1,29 @@
 import heapq
 
 def solution(jobs):
-    answer = 0
-    now = 0  # 현재시간
-    i = 0  # 처리된 작업 개수
-    start = -1  # 마지막 완료 시간
+    now = 0
+    ans = 0
+    count = 0
+    total_jobs = len(jobs)
     heap = []
-    
-    while i < len(jobs):
-        # 현재 시간에 시작할 수 있는 작업들을 힙에 추가
+
+    while count < total_jobs:
+        to_remove = []
         for job in jobs:
-            if start < job[0] <= now:
+            if job[0] <= now:
                 heapq.heappush(heap, [job[1], job[0]])
+                to_remove.append(job)
+
+        # 제거할 작업들을 한 번에 제거
+        for job in to_remove:
+            jobs.remove(job)
         
-        # 힙에 작업이 있다면, 가장 소요 시간이 짧은 작업을 꺼내서 처리
         if heap:
-            current = heapq.heappop(heap)
-            start = now
-            now += current[0]
-            answer += now - current[1]  # 요청으로부터 처리 시간
-            i += 1
+            current_job = heapq.heappop(heap)
+            now += current_job[0]
+            ans += now - current_job[1]
+            count += 1
         else:
-            # 현재 시간에 처리할 작업이 없다면 시간을 증가시킴
             now += 1
-    
-    return answer // len(jobs)
+
+    return int(ans // total_jobs)
