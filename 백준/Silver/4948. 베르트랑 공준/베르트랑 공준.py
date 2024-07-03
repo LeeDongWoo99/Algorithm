9d9ddc8a53abcb22f1dd31b2168889eaf0b7eea2
dongwooLee <dleh684@gmail.com>
2024-07-03 20:39:37 +0900

    [Silver II] Title: 베르트랑 공준, Time: 2704 ms, Memory: 110604 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -3,19 +3,19 @@ input = sys.stdin.readline
 
 def decimal(n):
     ans = 0
-    for i in range(n+1, 2*n + 1):
-        if i < 2:  # 2보다 작은 수는 소수가 아님
-            continue
-        elif i == 2:  # 2는 소수
-            ans += 1
-            continue
+    for i in range(n+1, 2*n +1):
         check = True
-        for j in range(2, int(i**0.5) + 1):  # 2는 이미 체크했으므로 홀수만 검사
+        if i < 2:
+            continue
+        elif i == 2:
+            ans +=1
+            continue
+        for j in range(2, int(i ** 0.5)+1):
             if i % j == 0:
                 check = False
                 break
         if check:
-            ans += 1
+              ans +=1
     return ans
 
 while True:
@@ -24,4 +24,10 @@ while True:
         break
     result = decimal(n)
     print(result)
+             
 
+
+        
+            
+            
+            
