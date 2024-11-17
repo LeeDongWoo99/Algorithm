22927781d58eadc5328e7b4a9ac9f04ea141682a
dongwooLee <dleh684@gmail.com>
2024-11-17 11:43:42 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 135 ms, Memory: 45,036 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,15 +1,12 @@
 T = 10
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # 덤프를 입력받는다.
-    dump = int(input())
-    # 1000개의 박스 높이를 입력받는다.
-    #for i in range(10):
+for tc in range(1, T+1):
+    N = int(input())
     lst = list(map(int, input().split()))
-    for _ in range(dump):
-        lst.sort(reverse=True)
-        lst[0] -=1
-        lst[-1] +=1
-
-    
-    print(f'#{test_case} {max(lst) - min(lst)}')
+    for _ in range(N):
+        lst.sort()
+        if lst[-1] - lst[0] <= 1:
+            break
+        lst[-1] -=1
+        lst[0] +=1
+    ans = max(lst) - min(lst)
+    print(f'#{tc} {ans}')
