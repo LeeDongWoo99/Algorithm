2d8284f09cc388c5319894cd8e0560f490ea9678
dongwooLee <dleh684@gmail.com>
2024-12-04 23:10:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 540 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,14 +1,15 @@
-n = int(input())  # 사용자로부터 n 입력받기
-cnt = 0  
-num = 1  
+N = int(input())
+st = 666
+count = 0
+lst = []
 
-while True:  
-    if "666" in str(num):  
-        cnt += 1  
-    if n == cnt:  
-        if n == 0: # 입력값을 0을 입력했을 때
-            print(666) # 0번째 종말의 수인 666을 출력
-        else:
-            print(num)  
-        break  
-    num += 1  
+while(True):
+    if "666" in str(st):
+        lst.append(st)
+        count += 1
+        if count == N:
+            break
+    st += 1
+
+
+print(lst[N-1])
