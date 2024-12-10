f92430aa7c64611c63d1d7f38ef23508630c0f6b
dongwooLee <dleh684@gmail.com>
2024-12-10 11:38:50 +0900

    [Silver I] Title: 회의실 배정, Time: 2808 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,15 @@
 N = int(input())
 I = [list(map(int, input().split())) for _ in range(N)]
 
+# 끝나는 시간 기준으로 정렬 (끝나는 시간이 같으면 시작 시간으로 정렬)
 I.sort(key=lambda x: (x[1], x[0]))
 
 ans = 0
 end_time = 0
 
 for start_time, finish_time in I:
-    if start_time >= end_time:
+    if start_time >= end_time:  # 현재 회의 시작 시간이 이전 회의 종료 시간 이후라면
         ans += 1
-        end_time = finish_time
+        end_time = finish_time  # 종료 시간을 갱신
+
 print(ans)
