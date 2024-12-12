9747baa99e3a74623a3ecaf04f25d4907e632cd1
dongwooLee <dleh684@gmail.com>
2024-12-12 17:15:38 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -3,13 +3,10 @@ m = n.split("-")
 ans = 0
 
 x = sum(map(int, (m[0].split("+"))))
-if n[0] == "-":
-    ans -= x
-else:
-    ans += x
+ans += x
 
 for x in m[1:]:
     x = sum(map(int, (x.split('+'))))
     ans -= x
-print(ans)
 
+print(ans)
