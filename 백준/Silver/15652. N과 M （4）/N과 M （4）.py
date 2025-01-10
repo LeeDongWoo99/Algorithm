81264ba94dc168cd0ea2c626236e1265e32118a5
dongwooLee <dleh684@gmail.com>
2025-01-10 17:01:53 +0900

    [Silver III] Title: N과 M (4), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(depth, i):
-    if depth == M:
+def dfs(depth, start):
+    if (depth == M):
         print(" ".join(map(str, arr)))
         return
-    for j in range(i, N + 1):
-        arr.append(j)
-        dfs(depth + 1, j)
+
+    for i in range(start, N + 1):
+        arr.append(i)
+        dfs(depth +1, i)
         arr.pop()
 
-N, M = map(int ,input().split())
-arr = []
 
+N, M = map(int, input().split())
+arr = []
 dfs(0, 1)
