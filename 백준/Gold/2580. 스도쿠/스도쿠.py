56554645e138f726b022f15f643488313c11a71d
dongwooLee <dleh684@gmail.com>
2025-01-10 19:59:22 +0900

    [Gold IV] Title: 스도쿠, Time: 2420 ms, Memory: 133280 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,46 +1,42 @@
 import sys
+
 input = sys.stdin.readline
 
-def possibility(row, col, value):
+
+def check(row, col, value):
     for i in range(9):
-        if value == arr[row][i]:
+        if (arr[row][i] == value or arr[i][col] == value):
             return False
 
-    for i in range(9):
-        if value == arr[i][col]:
-            return False
-
-    set_row = (row // 3) * 3
-    set_col = (col // 3) * 3
-
-    for i in range(set_row, set_row + 3):
-        for j in range(set_col, set_col + 3):
-            if arr[i][j] == value:
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if (arr[i][j] == value):
                 return False
-
     return True
 
-def sudoku(row, col):
-    if col == 9:
-        sudoku(row + 1, 0)
+
+def dfs(row, col):
+    if (col == 9):
+        dfs(row + 1, 0)
         return
 
-    if row == 9:
+    if (row == 9):
         for line in arr:
             print(*line)
         sys.exit(0)
 
-    if arr[row][col] == 0:
+    if (arr[row][col] == 0):
         for i in range(1, 10):
-            if possibility(row, col, i):
+            if (check(row, col, i)):
                 arr[row][col] = i
-                sudoku(row, col + 1)
-                arr[row][col] = 0
+                dfs(row, col + 1)
+        arr[row][col] = 0
         return
 
-    sudoku(row, col + 1)
-
+    dfs(row, col + 1)
 
 
 arr = [list(map(int, input().split())) for _ in range(9)]
-sudoku(0, 0)
+dfs(0, 0)
