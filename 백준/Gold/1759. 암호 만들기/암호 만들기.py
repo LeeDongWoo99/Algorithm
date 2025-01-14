c07e323ec51a5bfa786dd9ce66622770f24098f0
dongwooLee <dleh684@gmail.com>
2025-01-14 17:13:58 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+
+def check():
+    vowels_count = 0
+    consonants_count = 0
+    for char in ans:
+        if char in vowels:
+            vowels_count += 1
+        else: 
+            consonants_count += 1
+    return vowels_count >= 1 and consonants_count >= 2  
+
+
+def dfs(depth, start):
+    if depth == L:
+        if check():
+            print("".join(ans))
+        return
+
+    prev = ""
+    for i in range(start, C):
+        if apb[i] != prev:
+            ans.append(apb[i])
+            dfs(depth + 1, i + 1)
+            ans.pop()
+            prev = apb[i]
+
+
+L, C = map(int, input().split())
+apb = list(input().split())
+apb.sort()
+ans = []
+vowels = {'a', 'e', 'i', 'o', 'u'}
+
+dfs(0, 0)
