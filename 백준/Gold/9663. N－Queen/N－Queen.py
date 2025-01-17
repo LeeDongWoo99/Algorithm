00183fcdf1c8fe7642a0fef2105cfae9dbb0c662
dongwooLee <dleh684@gmail.com>
2025-01-17 12:03:37 +0900

    [Gold IV] Title: N-Queen, Time: 20868 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -10,7 +10,7 @@ def dfs(n):
         return
 
     for i in range(N):
-        if v1[i] == v2[n - i] == v3[n + i] == 0:
+        if v1[i] == 0 and v2[n - i] == 0 and v3[n + i] == 0:
             v1[i] = v2[n - i] = v3[n + i] = 1
             dfs(n + 1)
             v1[i] = v2[n - i] = v3[n + i] = 0
@@ -18,10 +18,8 @@ def dfs(n):
 
 N = int(input())
 ans = 0
-
 v1 = [0] * N
 v2 = [0] * N * 2
 v3 = [0] * N * 2
-
 dfs(0)
 print(ans)
