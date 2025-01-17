faf7bf49a456180ee05cc688f682236610979846
dongwooLee <dleh684@gmail.com>
2025-01-17 11:57:18 +0900

    [Gold IV] Title: 스도쿠, Time: 2164 ms, Memory: 133584 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,42 +1,45 @@
 import sys
-
 input = sys.stdin.readline
 
-
-def check(row, col, value):
+def possible(row, col, value):
+    # 행과 열에서 value가 중복되는지 확인
     for i in range(9):
-        if (arr[row][i] == value or arr[i][col] == value):
+        if value == arr[row][i] or value == arr[i][col]:
             return False
 
-    start_row = (row // 3) * 3
-    start_col = (col // 3) * 3
-    for i in range(start_row, start_row + 3):
-        for j in range(start_col, start_col + 3):
-            if (arr[i][j] == value):
+    # 3x3 블록에서 value가 중복되는지 확인
+    set_row = (row // 3) * 3
+    set_col = (col // 3) * 3
+    for i in range(set_row, set_row + 3):
+        for j in range(set_col, set_col + 3):
+            if arr[i][j] == value:
                 return False
+
     return True
 
-
 def dfs(row, col):
-    if (col == 9):
-        dfs(row + 1, 0)
-        return
-
-    if (row == 9):
+    # 모든 행을 탐색한 경우 출력 후 종료
+    if row == 9:
         for line in arr:
             print(*line)
         sys.exit(0)
 
-    if (arr[row][col] == 0):
-        for i in range(1, 10):
-            if (check(row, col, i)):
-                arr[row][col] = i
-                dfs(row, col + 1)
-        arr[row][col] = 0
+    # 새로운 행으로 넘어가는 처리
+    if col == 9:
+        dfs(row + 1, 0)
         return
 
-    dfs(row, col + 1)
+    # 빈 칸(0)인 경우 값을 시도
+    if arr[row][col] == 0:
+        for i in range(1, 10):
+            if possible(row, col, i):
+                arr[row][col] = i
+                dfs(row, col + 1)
+                arr[row][col] = 0
+    else:
+        # 빈 칸이 아닌 경우 다음 열로 이동
+        dfs(row, col + 1)
 
-
+# 입력받기
 arr = [list(map(int, input().split())) for _ in range(9)]
 dfs(0, 0)
