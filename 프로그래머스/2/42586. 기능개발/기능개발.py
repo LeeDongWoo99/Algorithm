92ac36cd48d3c219745a141b4b11478457831a0a
dongwooLee <dleh684@gmail.com>
2025-01-20 20:29:55 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,13 +1,12 @@
 def solution(progresses, speeds):
     ans = []
-    count = 0
     trial = 0
-    
+    count = 0
     while len(progresses) > 0:
-        if progresses[0] + speeds[0] * trial >= 100:
+        if (progresses[0] + speeds[0] * trial) >= 100:
+            count += 1
             progresses.pop(0)
             speeds.pop(0)
-            count += 1
         else:
             if count > 0:
                 ans.append(count)
@@ -15,4 +14,3 @@ def solution(progresses, speeds):
             trial += 1
     ans.append(count)
     return ans
-        
