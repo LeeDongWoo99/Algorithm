24cd725420c93f0b30eb7318a31169cfcde95dd1
dongwooLee <dleh684@gmail.com>
2025-01-21 20:24:56 +0900

    [level 2] Title: 더 맵게, Time: 1763.62 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,17 @@
 import heapq
 
 def solution(scoville, K):
-    heapq.heapify(scoville) 
     count = 0
-
-    while scoville[0] < K:  
+    heapq.heapify(scoville) # 최소힙으로 변환
+    
+    while scoville[0] < K:
         if len(scoville) < 2:
-            return -1  
-
+            return -1
+        
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
         new_scoville = first + (second * 2)
         heapq.heappush(scoville, new_scoville)
         count += 1
-
+        
     return count
