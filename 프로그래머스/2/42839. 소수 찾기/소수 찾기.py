129b88c042d0af27926965913324544fcb208ad3
dongwooLee <dleh684@gmail.com>
2025-01-23 21:23:57 +0900

    [level 2] Title: 소수 찾기, Time: 22.81 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,28 +1,25 @@
 from itertools import permutations
 
 def solution(numbers):
-    num = [x for x in numbers]
+    num = [ch for ch in numbers] 
     ans = []
     per = []
     
-    for i in range(1, len(num) + 1):
-        per += permutations(num, i)
-    lst = [int("".join(x)) for x in per]
+    for i in range(1, len(num) + 1): 
+        per+= list(permutations(num, i)) 
+    lst = [int(("").join(p)) for p in per] 
     
-    for n in lst:
-        if n < 2:
+    for n in lst:                            
+        if n < 2:       
             continue
         if n == 2:
             ans.append(n)
-        check = True
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
+        check = True           
+        for i in range(2,int(n**0.5) + 1): 
+            if n % i == 0:               
                 check = False
                 break
-        if check:
-            ans.append(n)
-    return len(set(ans))
-    
-    
-    answer = 0
-    return answer
+        if check: 
+            ans.append(n)                      
+
+    return len(set(ans)) 
