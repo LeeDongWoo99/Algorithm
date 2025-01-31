b1be018dcbee6fd6a1d406f007d5dc9790d5530b
dongwooLee <dleh684@gmail.com>
2025-01-31 10:57:17 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,16 +1,15 @@
 N, K = map(int, input().split())
 
 lst = []
-ans = 0 #K원을 만드는데 필요한 총 동전의 개수
-for _ in range(N): # N 크기 만큼 동전 종료 받기
+ans = 0
+for i in range(N):
     lst.append(int(input()))
+    
 
-
-for i in range(1, N+1):
-    if lst[-i] > K:
+for j in range(1, N + 1):
+    if K < lst[-j]:
         continue
-    ans += K // lst[-i]
-    K = K % lst[-i]
-    if K == 0:
-        break
+    ans += K // lst[-j]
+    K = K % lst[-j]
+    
 print(ans)
