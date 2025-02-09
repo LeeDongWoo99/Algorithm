725f5018eb62076caeddf3147e01d060ab16b9c7
dongwooLee <dleh684@gmail.com>
2025-02-09 16:16:10 +0900

    [level 2] Title: 피로도, Time: 44.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,18 @@
-def dfs(hp, idx, ans, dungeons):  # dungeons를 매개변수로 전달
-    global max_ans
-    if idx == len(dungeons):
-        max_ans = max(max_ans, ans)
-        return
-
-    for i in range(len(dungeons)):
-        if hp >= dungeons[i][0] and not visited[i]:  # visited[i] == False 대신 not 사용
-            visited[i] = True
-            dfs(hp - dungeons[i][1], idx + 1, ans + 1, dungeons)
-            visited[i] = False
-        else:
-            dfs(hp, idx + 1, ans, dungeons)
-
 def solution(k, dungeons):
-    global max_ans
-    max_ans = 0
-    global visited
-    visited = [False] * len(dungeons)  # 리스트 초기화 오류 수정
-    dfs(k, 0, 0, dungeons)  # dungeons를 매개변수로 전달
-    return max_ans
+    max_count = 0
+    
+    
+    def dfs(current_k, visited, count):
+        nonlocal max_count
+        max_count = max(max_count, count)
+        
+        for i in range(len(dungeons)):
+            if not visited[i] and current_k >= dungeons[i][0]:
+                visited[i] = True
+                dfs(current_k - dungeons[i][1], visited, count + 1)
+                visited[i] = False
+                
+    visited = [False] * len(dungeons)
+    dfs(k, visited, 0)
+    
+    return max_count
