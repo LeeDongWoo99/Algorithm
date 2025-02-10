78310f89fc06ebf308aae1b30c3f19ac09ae70ce
dongwooLee <dleh684@gmail.com>
2025-02-10 11:06:58 +0900

    [level 3] Title: 이중우선순위큐, Time: 269.25 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,29 @@
+import heapq
+
+def solution(operations):
+    min_heap = []
+    max_heap = []
+
+    for cmd in operations:
+        lst = cmd.split()
+        if lst[0] == "I":
+            num = int(lst[1])
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            
+        elif lst[0] == "D" and min_heap:
+            if lst[1] == "1":
+                max_num = -heapq.heappop(max_heap)
+                min_heap.remove(max_num)
+            elif lst[1] == "-1":
+                min_num = heapq.heappop(min_heap)
+                max_heap.remove(-min_num)
+                
+        if not min_heap and max_heap:
+            min_heap.clear()
+            max_heap.clear()
+            
+    if min_heap:
+        return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]  
+    else:
+        return [0, 0]
