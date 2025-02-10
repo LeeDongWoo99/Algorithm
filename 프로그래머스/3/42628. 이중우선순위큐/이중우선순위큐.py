75f729f1a7e5a283dcaebdd7e1c95d820e61a685
dongwooLee <dleh684@gmail.com>
2025-02-10 19:29:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 287.14 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,27 +3,24 @@ import heapq
 def solution(operations):
     min_heap = []
     max_heap = []
-
-    for cmd in operations:
-        lst = cmd.split()
-        if lst[0] == "I":
-            num = int(lst[1])
+    
+    for command in operations:
+        cmd = command.split()
+        
+        if cmd[0] == "I":
+            num = int(cmd[1])
             heapq.heappush(min_heap, num)
             heapq.heappush(max_heap, -num)
             
-        elif lst[0] == "D" and min_heap:
-            if lst[1] == "1":
-                max_num = -heapq.heappop(max_heap)
-                min_heap.remove(max_num)
-            elif lst[1] == "-1":
-                min_num = heapq.heappop(min_heap)
-                max_heap.remove(-min_num)
+        elif cmd[0] == "D" and min_heap:
+            if cmd[1] == "-1":
+                min_val = heapq.heappop(min_heap)
+                max_heap.remove(-min_val)
+            elif cmd[1] == "1":
+                max_val = -heapq.heappop(max_heap)
+                min_heap.remove(max_val)
                 
-        if not min_heap:
-            min_heap.clear()
-            max_heap.clear()
-            
-    if min_heap:
-        return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]  
+    if max_heap:
+        return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
     else:
         return [0, 0]
