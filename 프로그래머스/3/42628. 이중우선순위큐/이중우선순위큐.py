017b662b24235bcf121b79c7d75c5c82bfbacc9d
dongwooLee <dleh684@gmail.com>
2025-02-14 11:56:38 +0900

    [level 3] Title: 이중우선순위큐, Time: 259.31 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,24 @@
 import heapq
 
 def solution(operations):
-    min_heap = []
     max_heap = []
+    min_heap = []
     
     for command in operations:
         cmd = command.split()
-        
-        if cmd[0] == "I":
+        if cmd[0] == "I": # 명령어 첫 부분이 입력이라면
             num = int(cmd[1])
             heapq.heappush(min_heap, num)
             heapq.heappush(max_heap, -num)
-            
-        elif cmd[0] == "D" and min_heap:
-            if cmd[1] == "-1":
+        elif cmd[0] == "D":
+            if cmd[1] == "-1" and min_heap: # 명령어 뒷 부분이 -1이라면, 최소값 삭제
                 min_val = heapq.heappop(min_heap)
                 max_heap.remove(-min_val)
-            elif cmd[1] == "1":
+            if cmd[1] == "1" and min_heap: # 명령어 뒷 부분이 1이라면
                 max_val = -heapq.heappop(max_heap)
                 min_heap.remove(max_val)
-                
+            
     if max_heap:
         return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
     else:
-        return [0, 0]
+        return [0,0]
