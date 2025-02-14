04a3dbec819992867618e0b91a9ea1d7284989d1
dongwooLee <dleh684@gmail.com>
2025-02-14 15:08:25 +0900

    [Platinum V] Title: 히스토그램, Time: 148 ms, Memory: 45240 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for _ in range(N):
+    num = int(input())
+    lst.append(num)
+    
+stack = []
+max_rst = 0
+
+for i in range(N):
+    idx = i
+    
+    while stack and stack[-1][1] > lst[i]:
+        idx, height = stack.pop()
+        rst = (i - idx) * height
+        max_rst = max(max_rst, rst)
+    stack.append([idx, lst[i]])
+    
+while stack:
+    idx, height = stack.pop()
+    rst = (N - idx) * height
+    max_rst = max(max_rst, rst)
+    
+print(max_rst)
