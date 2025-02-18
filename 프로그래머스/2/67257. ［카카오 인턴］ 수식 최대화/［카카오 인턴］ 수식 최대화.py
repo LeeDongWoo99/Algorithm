e3a7b6e4fbe57360dd9d443cfbd137025be5ff80
dongwooLee <dleh684@gmail.com>
2025-02-18 15:12:51 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,41 +1,43 @@
 from itertools import permutations
 
-def operation(num1, num2, op):   # 연산자에 따라 수식을 계산해주는 함수
+def operation(num1, num2, op):  
+    """ 연산자에 따라 수식을 계산하는 함수 """
     if op == '+':
         return str(int(num1) + int(num2))
     if op == '-':
         return str(int(num1) - int(num2))
     if op == '*':
         return str(int(num1) * int(num2))
-    
-def calculate(exp,op):   # 연산자 우선순위에 맞게 계산해주는 함수
-    array=[]
-    tmp=""
-    for i in exp:
-        if i.isdigit()==True:
-            tmp+=i
-        else:
-            array.append(tmp)
-            array.append(i)
-            tmp=""
-    array.append(tmp)
-    
-    for o in op:
-        stack=[]
-        while len(array)!=0:
-            tmp=array.pop(0)
-            if tmp==o:
+
+def calculate(exp, op_order):  
+    """ 연산자 우선순위에 맞게 계산하는 함수 """
+    array = exp[:]  # 원본 리스트 복사
+    for o in op_order:
+        stack = []
+        while array:
+            tmp = array.pop(0)
+            if tmp == o:
                 stack.append(operation(stack.pop(), array.pop(0), o))
             else:
                 stack.append(tmp)
-        array=stack
-            
+        array = stack
     return abs(int(array[0]))
 
+def parse_expression(expression):
+    """ 주어진 수식을 숫자와 연산자로 분리하는 함수 """
+    tokens = []
+    tmp = ""
+    for char in expression:
+        if char.isdigit():
+            tmp += char
+        else:
+            tokens.append(tmp)
+            tokens.append(char)
+            tmp = ""
+    tokens.append(tmp)
+    return tokens
+
 def solution(expression):
-    op = ['+', '-', '*']
-    op = list(permutations(op, 3))
-    result=[]
-    for i in op:
-        result.append(calculate(expression, i))
-    return max(result)
+    op_orders = list(permutations(['+', '-', '*'], 3))  # 연산자 우선순위 조합
+    tokens = parse_expression(expression)  # 숫자와 연산자 분리 (한 번만 수행)
+    return max(calculate(tokens, op_order) for op_order in op_orders)
