67166806528f2c19b7308d40951a39f13a90720c
dongwooLee <dleh684@gmail.com>
2025-02-22 09:36:22 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,13 @@
 def solution(n, lost, reserve):
+    
+    reserve_set = set(reserve) - set(lost)
     lost_set = set(lost) - set(reserve)
-    reverse_set = set(reserve) - set(lost)
     
-    for r in sorted(reverse_set):
-        if r - 1 in  lost_set:
+    for r in sorted(reserve_set):
+        if r - 1 in lost_set:
             lost_set.remove(r - 1)
-        elif r + 1 in lost_set:  # 뒷번호 학생에게 빌려줌
+        elif r + 1 in lost_set:
             lost_set.remove(r + 1)
-    
-    # 전체 학생 수 - 체육복을 못 빌린 학생 수
+        
     return n - len(lost_set)
+    
