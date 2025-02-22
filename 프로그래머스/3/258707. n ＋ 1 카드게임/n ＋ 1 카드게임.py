99f30f51f01bb1e00354751d5357e44f7ce2c869
dongwooLee <dleh684@gmail.com>
2025-02-22 13:24:54 +0900

    [level 3] Title: n + 1 카드게임, Time: 166.15 ms, Memory: 7.83 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,34 +1,36 @@
-def solution(coin, cards):
-    ans = 1
-    n = len(cards)
-    my_card = cards[:n // 3] # 뽑을 카드 뭉치에 n // 3 개만 내 카드
-    draw_index = n // 3 # 라운드 별로 뽑을 카드에 시작 인덱스
-    drawn_cards= []
-    
-    while coin >= 0 and draw_index < n - 1:
-        drawn_cards.extend(cards[draw_index :draw_index +2])
-        
-        if possible(my_card, my_card, n + 1):
-            ans += 1
-            print("my_card", my_card)
-        elif possible(my_card, drawn_cards, n + 1) and coin >= 1:
-            coin -= 1
-            ans += 1
-        elif possible(drawn_cards, drawn_cards, n + 1) and coin >= 2:
-            coin -= 2
-            ans += 1
-        else:
-            break
-            
-        draw_index += 2
-        
-    return ans
-
-def possible(mycard, pluscard, t):
-    for card in mycard[:]:
-        tc = t - card
-        if tc in pluscard:
-            mycard.remove(card)
-            pluscard.remove(tc)
+def remove_matching_pair(player_cards, available_cards, target_sum):
+    for card in player_cards[:]:  
+        matching_card = target_sum - card
+        if matching_card in available_cards:
+            player_cards.remove(card)
+            available_cards.remove(matching_card)
             return True
     return False
+            
+def solution(coin, cards):
+    answer = 1  # 첫 번째 라운드는 무조건 진행
+    n = len(cards)
+    player_hand = cards[:n//3]  # 초기 보유 카드 
+    draw_index = n // 3  # 남은 카드에서 가져올 첫 번째 인덱스
+    drawn_cards= []  # 뽑은 카드 저장 리스트
+    
+    while coin >= 0 and draw_index < n - 1:  
+        # 뽑을 카드 2장 추가
+        drawn_cards.extend(cards[draw_index :draw_index +2]) 
+
+        # 내 카드끼리 짝을 맞출 수 있다면, 그대로 진행
+        if remove_matching_pair(player_hand, player_hand, n + 1):
+            pass
+        # 내 카드와 남은 카드에서 짝을 맞출 수 있으면, 코인 1개를 사용
+        elif coin >= 1 and remove_matching_pair(player_hand, drawn_cards, n + 1):
+            coin -= 1
+        # 남은 카드끼리 짝을 맞출 수 있으면, 코인 2개를 사용
+        elif coin >= 2 and remove_matching_pair(drawn_cards, drawn_cards, n + 1):
+            coin -= 2
+        else:
+            break
+        
+        answer += 1
+        draw_index += 2 
+    
+    return answer
