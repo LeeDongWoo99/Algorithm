f041a8891d8cba512f4c8f49b1dc78f697eb7531
dongwooLee <dleh684@gmail.com>
2025-05-21 10:16:05 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 117 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,37 +1,28 @@
 def dfs(n):
-    # 교환 횟수가 최대 교환 횟수일 때
     global ans
     if n == N:
-        ans = max(ans, int(''.join(map(str, lst))))
+        ans = max(ans, int("".join(lst)))
         return
 
-    for i in range(L-1):
-        for j in range(i+1, L):
-            lst[i], lst[j] = lst[j],lst[i]
-
-            chk = int(''.join(map(str, lst))) *10 + n
+    for i in range(L - 1):
+        for j in range(i + 1, L):
+            lst[i], lst[j] = lst[j], lst[i]
+            chk = int("".join(lst)) + n
             if chk not in v:
-                dfs(n+1)
                 v.append(chk)
+                dfs(n + 1)
             lst[i], lst[j] = lst[j], lst[i]
 
-
-
-tc = int(input())
-
-for tc in range(1, tc+1):
-    st, t = input().split()
-    # 최대 교환 횟수
-    N = int(t)
-    lst = []
-    # 문자열을 리스트로 변환
-    for ch in st:
-        lst.append(ch)
-    # 방문 리스트
-    v = []
-    # 최대값
+t = int(input()) # 테스트 개수
+for tc in range(1 , t + 1):
+    st, c = input().split()
     ans = 0
-    # 교환 횟수
+    lst = []
+    N = int(c)
+    for i in st:
+        lst.append(i)
     L = len(lst)
+    v = [] # 방문 배열
     dfs(0)
-    print(f"#{tc} {ans}")
+
+    print(f'#{tc} {ans}')
