bbc4b9c0c9d606d7e1e6ae05ee0d7a411823e234
dongwooLee <dleh684@gmail.com>
2025-08-24 12:03:36 +0900

    [level 2] Title: 구명보트, Time: 19.93 ms, Memory: 9.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,22 +1,12 @@
 def solution(people, limit):
-    # 내림차순으로 정렬
-    people.sort(reverse=True)
-    # 가장 무거운 사람을 가리키는 포인터
-    left = 0
-    # 가장 가벼운 사람을 가리키는 포인터, 인덱스의 끝 번호는 리스트 크기 -1을 해줘야 한다.
-    right = len(people) - 1
-    # 필요한 구명보트의 수
+    people.sort()  # 오름차순
+    left, right = 0, len(people) - 1
     result = 0
 
-    # 두 포인터가 서로 교차할 때까지 반복합니다.
     while left <= right:
-        # 가장 가벼운 사람과 가장 무거운 사람의 무게 합이 제한 이내라면
         if people[left] + people[right] <= limit:
-            # 가벼운 사람도 보트에 탑니다.
-            right -= 1
-        # 무거운 사람은 항상 보트에 탑니다.
-        left += 1
-        # 보트 하나 추가
+            left += 1
+        right -= 1
         result += 1
 
     return result
