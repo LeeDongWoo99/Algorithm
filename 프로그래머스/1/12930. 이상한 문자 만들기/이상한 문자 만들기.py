a2fde9ad6ade495f208cd156f193b139f51df6b0
dongwooLee <dleh684@gmail.com>
2025-08-24 13:48:23 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,20 @@
+import re
+
 def solution(s):
-    st = s.split(" ")
-    new_words = []
-    for word in st:
-        new_word = ''
-        for i in range(len(word)):
-            # 짝수인 경우
-            if i % 2 == 0:
-                new_word += word[i].upper()
-            # 홀수인 경우
-            else:
-                new_word += word[i].lower()
-        new_words.append(new_word)
-    return ' '.join(new_words)
+    words = re.split(r'(\s+)', s)
+    result = []
+    for word in words:
+        if word.isspace():
+            result.append(word)
+        else:
+            new_word = ""
+            for i in range(len(word)):
+                if i % 2 == 0:
+                    new_word += word[i].upper()
+                else:
+                    new_word += word[i].lower()
+            result.append(new_word)
+    return ''.join(result)
+
+# 테스트
+print(solution("hello   world"))  # "HeLlO   WoRlD"
